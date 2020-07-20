6cb63a658052f8f176fceecf8107ed5dc8640a66
Nightscape Coffee <0c48d607@opayq.com>
2020-07-19 17:32:13 -0700

    Update player count with sitenav

--- docs/assets/serverstatus-home.js
+++ docs/assets/serverstatus-home.js
@@ -8,6 +8,7 @@
         // siteNav dependent
         {% if site.siteNav %}
         document.getElementById("serverInd").classList.add("text-green-600");
+        document.getElementById("serverPlaynow").innerHTML = `<span class="inline-block bg-yellow-500 rounded-full px-3 py-1 text-sm font-thin uppercase text-gray-900 mr-2 shadow-lg">Play Now: ${data.players.online} Online</span>`;
         {% else %}
         document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;We're online!&ensp;&middot;&ensp;${data.players.online} playing</p>`;
         document.getElementById("serverStatusM").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;Online, ${data.players.online} playing</p>`;
