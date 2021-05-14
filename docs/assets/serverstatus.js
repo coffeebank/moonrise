721b669e8e2d2b336b86edf887e616a01307431f
Nightscape Coffee <0c48d607@opayq.com>
2021-05-14 03:06:59 -0700

    Fix status for non-siteNav pages

--- docs/assets/serverstatus.js
+++ docs/assets/serverstatus.js
@@ -3,9 +3,18 @@
 {% include serverstatus.js %}
 .then(data => {
     if (data.online == true) {
+        {% if site.siteNav %}
         document.getElementById("serverInd").classList.add("text-green-600");
+        document.getElementById("serverPlaynow").innerHTML = `<span class="inline-block bg-yellow-500 rounded-full px-3 py-1 text-sm font-thin uppercase text-gray-900 mr-2 shadow-lg">Play Now: ${data.players.online} Online</span>`;
+        {% else %}
+        document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;We're online!&ensp;&middot;&ensp;${data.players.online} playing</p>`;
+        {% endif %}
     }
     else {
+        {% if site.siteNav %}
         document.getElementById("serverInd").classList.add("text-red-600");
+        {% else %}
+        document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-red-600">&#11044;</span>&emsp;Sorry! We're offline...</p>`;
+        {% endif %}
     }
 });
